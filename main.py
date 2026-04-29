4056dedbc134d7b91e1ed7772cea5c7c3efd9649
Laisczt <laissopaes@gmail.com>
2026-04-29 14:58:42 -0300

    add vinculum, fracoes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 import os
 from argparse import ArgumentParser, RawTextHelpFormatter
-from symbols import getSymbols, clockFace, pleaseJupiter
+from symbols import *
 
 class color:
     BOLD = '\033[1m'
+    BOLD_END = '\033[22m'
+    OVERLINE = '\033[53m'
+    OVERLINE_END = '\033[55m'
     RESET = '\033[0m'
     UNDERLINE = '\033[4m'
     PURPLE = '\033[95m'
@@ -30,9 +33,15 @@ def getPrintable(reps, lowercase, unicode):
 
 
 def DEBUG_PRINT_SYMBOLS(listasimbolos):
+    print(F"GERADOS {len(listasimbolos)} SIMBOLOS:")
     for s in listasimbolos:
-        print(f'{s["valor"]:<6} : {getPrintable(s["reps"],False, False)}')
+        if type(s['valor']) == int:
+            print(f'{s["valor"]:<8} : {getPrintable(s["reps"],False, False)}')
+        else:
+            print(f'{s["valor"]:.6f} : {getPrintable(s["reps"],False, False)}')
+
 def DEBUG_PRINT_ARGUMENTS(args):
+    print("ARGUMENTOS:")
     for k, v in vars(args).items():
         if k not in ['input', 'export'] and v:
             print(k)
@@ -63,7 +72,14 @@ def checkIncompat(argumentos, incompatibilidades):
 
 
 def main():
-    parser = ArgumentParser(description="Conversor de números decimais em algarismos romanos", epilog='Criado por: Laisczt, Sunamit\nMaio de 2026', formatter_class=RawTextHelpFormatter, fromfile_prefix_chars="@")
+    parser = ArgumentParser(
+        description=f"""Conversor de números decimais em algarismos romanos\n
+        notas:\n
+         - o programa não limita o tamanho da entrada, isso pode resultar em longas repetições do maior símbolo (e.g. MMMMMMMMMMM = 11_000)\n
+         - duas maneiras de representar números grandes estão disponíveis: vinculum({color.BOLD}-v{color.BOLD_END}, {color.BOLD}-V{color.BOLD_END}) e apostrophus ({color.BOLD}-b{color.BOLD_END})""", 
+        epilog='Criado por: Laisczt, Sunamit\nMaio de 2026', 
+        formatter_class=RawTextHelpFormatter, 
+        fromfile_prefix_chars="@")
 
     # Número de entrada
     parser.add_argument('input', help='number to be converted, in decimal notation')
@@ -72,37 +88,42 @@ def main():
     parser.add_argument('--export', help='exportar configurações de estilo pro arquivo')
 
     # Flags
-    parser.add_argument('-a', '--additive', action='store_true', help=f'utiliza a forma aditiva (e.g XXXX ao invés de XL)\n *implica {color.BOLD}-N{color.RESET} e {color.BOLD}-F{color.RESET}')
-    parser.add_argument('-A', '--additive-long', action='store_true', help=f'exclui V,L,D,etc. (símbolos com valores decimais começando com 5), optando por usar a forma aditiva (e.g iiiiiii ao invés de 7)\n *implica {color.BOLD}-a{color.RESET}\n *incompatível com {color.BOLD}-s{color.RESET}, {color.BOLD}-S{color.RESET}, {color.BOLD}-d{color.RESET}')
+    parser.add_argument('-a', '--additive', action='store_true', help=f'utiliza a forma aditiva (e.g XXXX ao invés de XL)\n *implica {color.BOLD}-N{color.BOLD_END} e {color.BOLD}-F{color.BOLD_END}')
+    parser.add_argument('-A', '--additive-long', action='store_true', help=f'exclui V,L,D,etc. (símbolos com valores decimais começando com 5), optando por usar a forma aditiva (e.g iiiiiii ao invés de 7)\n *implica {color.BOLD}-a{color.BOLD_END}\n *incompatível com {color.BOLD}-s{color.BOLD_END}, {color.BOLD}-S{color.BOLD_END}, {color.BOLD}-d{color.BOLD_END}')
     parser.add_argument('-f', '--additive-four', action='store_true', help='utiliza forma aditiva para representar o valor 4 (IIII)')
-    parser.add_argument('-F', '--additive-fours', action='store_true', help=f'utiliza forma aditiva para representar os valores 4, 40, e 400\n *implica {color.BOLD}-f{color.RESET}')
+    parser.add_argument('-F', '--additive-fours', action='store_true', help=f'utiliza forma aditiva para representar os valores 4, 40, e 400\n *implica {color.BOLD}-f{color.BOLD_END}')
     parser.add_argument('-J', '--please-jupiter', action='store_true', help='utiliza a forma aditiva para representar o valor 4 (IIII), somente se a entrada for exatamente 4')
     parser.add_argument('-n', '--additive-nine', action='store_true', help='utiliza forma aditiva para representar o valor 9 (VIIII)')
-    parser.add_argument('-N', '--additive-nines', action='store_true', help=f'utiliza forma aditiva para representar os valores 9, 90, e 900\n *implica {color.BOLD}-n{color.RESET}')
-    parser.add_argument('-s', '--subtractive-forms', action='store_true', help=f'permite representações subtrativas não padrão (e.g IC, XM, XD)\n *incompatível com {color.BOLD}-A{color.RESET}')
-    parser.add_argument('-S', '--subtractive-long', action='store_true', help=f'permite representações subtrativas com até 3 caractéres em sequência\n *incompatível com {color.BOLD}-A{color.RESET}')
-    parser.add_argument('-d', '--subtractive-fives', action='store_true', help=f'permite que não-potências-de-cinco (V,L,D) sejam utilizados subtrativamente, com até 1 caractér em sequência\n *incompatível com {color.BOLD}-A{color.RESET}')
+    parser.add_argument('-N', '--additive-nines', action='store_true', help=f'utiliza forma aditiva para representar os valores 9, 90, e 900\n *implica {color.BOLD}-n{color.BOLD_END}')
+    parser.add_argument('-s', '--subtractive-forms', action='store_true', help=f'permite representações subtrativas não padrão (e.g IC, XM, XD)\n *incompatível com {color.BOLD}-A{color.BOLD_END}')
+    parser.add_argument('-S', '--subtractive-long', action='store_true', help=f'permite representações subtrativas com até 3 caractéres em sequência\n *incompatível com {color.BOLD}-A{color.BOLD_END}')
+    parser.add_argument('-d', '--subtractive-fives', action='store_true', help=f'permite que não-potências-de-cinco (V,L,D) sejam utilizados subtrativamente, com até 1 caractér em sequência\n *incompatível com {color.BOLD}-A{color.BOLD_END}')
     parser.add_argument('-c', '--clock', action='store_true', help='caso a entrada seja de 1-12, utiliza caractéres utf8 dedicados')
-    parser.add_argument('-u', '--unicode', action='store_true', help=f'utiliza caractéres utf8. Exclui caracteres dedicados da notação apostrophus\n *veja {color.BOLD}-B{color.RESET}({color.BOLD}--apostrophus-special{color.RESET})')
+    parser.add_argument('-u', '--unicode', action='store_true', help=f'utiliza caractéres utf8 para símbolos latinos (quando possível). Exclui caracteres dedicados da notação apostrophus\n *veja {color.BOLD}-B{color.BOLD_END}({color.BOLD}--apostrophus-special{color.BOLD_END})')
     parser.add_argument('-l', '--lowercase', action='store_true', help='utiliza versões minúsculas dos símbolos (onde possível)')
     parser.add_argument('-0', '--nulla', action='store_true', help='caso a entrada for 0, imprime a palavra Nulla (o comportamento padrão é imprimir N)')
-    parser.add_argument('-i', '--implied-fractions', action='store_true', help=f'não imprime parte fracional, mas risca o último dígito para indicar existência de fração (e.g {color.CROSS}I{color.RESET},{color.CROSS}M{color.RESET})')
+    parser.add_argument('-i', '--implied-fractions', action='store_true', help=f'não imprime parte fracional, mas risca o último dígito para indicar existência de fração (e.g {color.CROSS}I{color.BOLD_END},{color.CROSS}M{color.BOLD_END}) *incompatível com {color.BOLD}-r{color.BOLD_END} e {color.BOLD}-R{color.BOLD_END}')
     parser.add_argument('-r', '--limited-fractions', action='store_true', help='arredonda valores fracionais a frações de 12 (Uncias)')
-    parser.add_argument('-R', '--expand-fractions', action='store_true', help='não utiliza caractéres compactos para uncias')
-    parser.add_argument('-b', '--apostrophus', action='store_true', help='utiliza notação apostrophus para números grandes')
-    parser.add_argument('-B', '--apostrophus-special', action='store_true', help=f'utiliza símbolos utf8 dedicados para notação apostrophus\n *implica {color.BOLD}-b{color.RESET}')
+    parser.add_argument('-R', '--expanded-fractions', action='store_true', help='não utiliza caractéres compactos para uncias')
+    parser.add_argument('-v', '--vinculum', action='store_true', help=f'utiliza notação vinculum (aka titulum) para números grandes. Inclui multiplicação por 1000\n *veja também {color.BOLD}-V{color.BOLD_END}')
+    parser.add_argument('-V', '--vinculum-large', action='store_true', help=f'utiliza notação vinculum (aka titulum) para números grandes. Inclui multiplicação por 1000 e 100_000\n *implica {color.BOLD}-v{color.BOLD_END}\n *incompatível com {color.BOLD}-b{color.BOLD_END}')
+    parser.add_argument('-b', '--apostrophus', action='store_true', help=f'utiliza notação apostrophus para números grandes\n *veja também {color.BOLD}-B{color.BOLD_END}\n *incompatível com {color.BOLD}-v{color.BOLD_END}')
+    parser.add_argument('-B', '--apostrophus-special', action='store_true', help=f'utiliza símbolos utf8 dedicados para notação apostrophus\n *implica {color.BOLD}-b{color.BOLD_END}')
 
     implicacoes = { # a opção à esquerda implica todas à direita (USE _ AO INVES DE -)
         "additive" : ("additive_fours", "additive_nines"),
         "additive_long" : tuple(["additive"]),
         "additive_fours" : tuple(["additive_four"]),
         "additive_nines" : tuple(["additive_nine"]),
-        "apostrophus_special": tuple(["apostrophus"])
+        "apostrophus_special": tuple(["apostrophus"]),
+        "vinculum_large": tuple(["vinculum"])
     }
 
     incompat = { # cada opção à esquerda é incompatível com cada uma à direita
         tuple(['additive_long']): ('subtractive_forms', 'subtractive_fives', 'subtractive_long'),
-        tuple(['additive_nine']): tuple(['subtractive_long'])
+        tuple(['additive_nine']): tuple(['subtractive_long']),
+        tuple(['implied_fractions']): ('expanded_fractions', 'limited_fractions'),
+        tuple(['vinculum']): tuple(['apostrophus'])
     }
 
     a = parser.parse_args() # argumentos podem ser acessados com a.nomeArg
@@ -112,18 +133,18 @@ def main():
     hasIncompat = checkIncompat(a, incompat)# checa se existem incompatibilidades entre flags
 
     if(hasIncompat): 
-        raise ValueError(f"Opções incompatíveis: {color.BOLD + color.RED}{"--"+hasIncompat[0]}{color.RESET + color.PURPLE}, {color.BOLD + color.RED}{"--"+hasIncompat[1]}{color.RESET + color.PURPLE}")
+        raise ValueError(f"Opções incompatíveis: {color.BOLD + color.RED}{"--"+hasIncompat[0]}{color.BOLD_END + color.PURPLE}, {color.BOLD + color.RED}{"--"+hasIncompat[1]}{color.BOLD_END + color.PURPLE}")
 
     try:    # Verifica se a entrada é numérica
         float(a.input)
     except:
-        raise ValueError(f"Entrada deve ser numérica: {color.RED}{a.input}{color.RESET + color.PURPLE}")
+        raise ValueError(f"Entrada deve ser numérica: {color.RED}{a.input}{color.BOLD_END}")
 
 
     if(a.export): # exporta configurações de estilo à arquivo
         path = a.export
         if(os.path.isdir(path)):
-            raise ValueError(f"Caminho provido para export é um diretório: {color.RED}{a.export}{color.RESET + color.PURPLE}")
+            raise ValueError(f"Caminho provido para export é um diretório: {color.RED}{a.export}{color.BOLD_END}")
         
         with open(path, 'w') as f:
             for arg, val in vars(a).items():
@@ -131,22 +152,6 @@ def main():
                 if not val: continue
                 f.write("--" + arg.replace("_", "-") + "\n")
 
-
-    symbols = getSymbols(   # Símbolos a serem usados com o algorítmo do contador
-        a.apostrophus,
-        a.apostrophus_special,
-        a.additive_long,
-        a.additive_four,
-        a.additive_fours,
-        a.additive_nine,
-        a.additive_nines,
-        a.subtractive_forms,
-        a.subtractive_long,
-        a.subtractive_fives,
-    )
-
-    clock = clockFace() # Lista de números de 1-12, use caso o usuário utilize a opção -c/--clock e se o valor de entrada seja de 1-12
-    jupiter = pleaseJupiter() # Lista contendo somente IIII, use caso o usuário utiliza a opção -j/--please-jupiter, e se o valor de entrada seja exatamente 4
 
     #   para SUNAMITA
     #   
@@ -158,7 +163,7 @@ def main():
     #   o valor a ser convertido está em `a.input` (precisa converter para float)
     #   além você só deve precisar usar `a.lowercase`, `a.unicode`, `a.please_jupiter`, `a.nulla`, `a.clock` e `a.implied_fractions` (TODOS SÃO BOOLEANOS)
     #   de uma olhada no -h para saber o que cada uma faz, qualquer dúvida pode perguntar
-    #   para o `a.implied_fractions`, arredonde o número para cima e risque o último símbolo. faça assim: color.CROSS + simbolo + color.RESET
+    #   para o `a.implied_fractions`, arredonde o número para cima e risque o último símbolo. faça assim: color.CROSS + simbolo + color.BOLD_END
     #
     #   lista de símbolos está na variável symbols(em ordem decrescente do valor)
     #   para saber o valor de um símbolo use `symbol[ind]['valor']`
@@ -170,10 +175,30 @@ def main():
     #   `DEBUG_PRINT_SYMBOLS(symbols)` para imprimir os símbolos
     #   `DEBUG_PRINT_ARGUMENTS(a)` para imprimir os argumentos
 
-    #DEBUG_PRINT_SYMBOLS(symbols)
-    #DEBUG_PRINT_ARGUMENTS(a)
+    symbols = generateSymbols(   # Símbolos a serem usados com o algorítmo do contador
+        a.apostrophus,
+        a.apostrophus_special,
+        a.vinculum,
+        a.vinculum_large,
+        a.additive_long,
+        a.additive_four,
+        a.additive_fours,
+        a.additive_nine,
+        a.additive_nines,
+        a.subtractive_forms,
+        a.subtractive_long,
+        a.subtractive_fives,
+        a.implied_fractions,
+        a.limited_fractions,
+        a.expanded_fractions
+    )
+
+    clock = getClockFace() # Lista de números de 1-12, use caso o usuário utilize a opção -c/--clock e se o valor de entrada seja de 1-12
+    jupiter = getPleaseJupiter() # Lista contendo somente IIII, use caso o usuário utiliza a opção -j/--please-jupiter, e se o valor de entrada seja exatamente 4
+
+    DEBUG_PRINT_ARGUMENTS(a)
+    DEBUG_PRINT_SYMBOLS(symbols)
+     
 
 if __name__ == "__main__":
     main()
-
-
--- symbols.py
+++ symbols.py
@@ -1,5 +1,5 @@
 
-def subtract(left, right, amount):
+def _subtract(left, right, amount):
     value = right['valor'] - left['valor'] * amount
     reps = []
 
@@ -8,14 +8,17 @@ def subtract(left, right, amount):
 
     return [{'valor': value, 'reps': tuple(reps)}]
 
-def subtractUpTo(left, right, amount):
+def _subtractUpTo(left, right, amount):
     res = []
     for i in range(amount):
-        res += subtract(left, right, i+1)
+        res += _subtract(left, right, i+1)
     return res
 
-def getSymbols( apostrophus,
+def generateSymbols( 
+                apostrophus,
                 apostrophus_special,
+                vinculum,
+                vinculum_large,
                 additive_long,
                 additive_four,
                 additive_fours,
@@ -23,10 +26,12 @@ def getSymbols( apostrophus,
                 additive_nines,
                 subtractive_forms,
                 subtractive_long,
-                subtractive_fives):
+                subtractive_fives,
+                implied_fractions,
+                limited_fractions,
+                expanded_fractions):
 
     symbols = []
-
     tens = [
         {"valor": 1, "reps":("I", "i", "Ⅰ", "ⅰ")},
         {"valor": 10, "reps":("X", "x", "Ⅹ", "ⅹ")},
@@ -38,12 +43,13 @@ def getSymbols( apostrophus,
         {"valor": 50, "reps":("L", "l", "Ⅼ", "ⅼ")}
     ] if not additive_long else []
 
+    subs = []
 
-    if not apostrophus:
+    if not (vinculum or apostrophus):
         tens += [{"valor": 1000, "reps":("M", "m", "Ⅿ", "ⅿ")}]
         fives += [{"valor": 500, "reps":("D", "d", "Ⅾ", "ⅾ")}] if not additive_long else []
 
-    else:
+    elif apostrophus:
         if not apostrophus_special:
             tens += [
                 {"valor": 1000, "reps":("CIↃ", "ciↄ", "ⅠↃ", "ⅰↄ")},
@@ -67,40 +73,92 @@ def getSymbols( apostrophus,
                 {"valor": 50000, "reps":("ↇ", "ↇ", "ↇ", "ↇ")}
             ] if not additive_long else []
 
-    symbols += tens + fives
 
     for t in range(len(tens)-1): # subtract tens from tens
         for t2 in range(t+1, len(tens)):
             if(t == 0 and t2 == 1 and additive_nine): continue
             if(t2-t == 1 and additive_nines): continue
             if(t2 - t >= 2 and not subtractive_forms): continue
-            symbols += subtractUpTo(tens[t], tens[t2], 1 if not subtractive_long else 3)
+            subs += _subtractUpTo(tens[t], tens[t2], 1 if not subtractive_long else 3)
 
     for t in range(len(tens)-1): # subtract tens from fives
         for f in range(t, len(fives)):
             if(t == 0 and f == 0 and additive_four): continue
             if(t == f and additive_fours): continue
             if(t != f and not subtractive_forms): continue
-            symbols += subtractUpTo(tens[t], fives[f], 1 if (t==f or not subtractive_long) else 3)
+            subs += _subtractUpTo(tens[t], fives[f], 1 if (t==f or not subtractive_long) else 3)
     
     if(subtractive_fives):
         for f in range(len(fives)-1): # subtract fives from tens
             for t in range(f+2, len(tens)):
                 if(t-f != 2 and not subtractive_forms): continue
-                symbols += subtract(fives[f], tens[t], 1)
+                subs += _subtract(fives[f], tens[t], 1)
         for f in range(len(fives)-1): # subtract fives from fives
             for f2 in range(f+1, len(fives)):
                 if(f2 - f != 1 and not subtractive_forms): continue
-                symbols += subtract(fives[f], fives[f2], 1)
+                subs += _subtract(fives[f], fives[f2], 1)
+
+
+    symbols += tens + fives + subs
+
+    vinc = []
+
+    if vinculum:
+        for s in symbols:
+            newsymbol = {}
+            newsymbol['valor'] = s['valor'] * 1000
+            reps = []
+            for r in s['reps']:
+                reps.append("\033[53m" + r + '\033[55m')
+            newsymbol['reps'] = tuple(reps)
+            vinc.append(newsymbol)
+        
+        if vinculum_large:
+            for s in symbols:
+                newsymbol = {}
+                newsymbol['valor'] = s['valor'] * 100_000
+                reps = []
+                for r in s['reps']:
+                    reps.append("|\033[53m" + r + '\033[55m|')
+                newsymbol['reps'] = tuple(reps)
+                vinc.append(newsymbol) 
+
+    symbols += vinc
+
+    fracs = []
+
+    if not implied_fractions:
+        fracs = [
+            {"valor": 1/12, "reps": ("·","·","·","·")},
+            {"valor": 6/12, "reps": ("S","s","S","s")}
+        ]
+        if not expanded_fractions:
+            fracs += [
+                {"valor": 2/12, "reps": (":",":",":",":")},
+                {"valor": 3/12, "reps": ("∴","∴","∴","∴")},
+                {"valor": 4/12, "reps": ("∷","∷","∷","∷")},
+                {"valor": 5/12, "reps": ("⁙","⁙","⁙","⁙")}
+            ]
+
+        if not limited_fractions:
+            fracs += [
+                {"valor": 1/24, "reps": ("Є","є","Є","є")},
+                {"valor": 1/48, "reps": ("~Ↄ","~ↄ","~Ↄ","~ↄ")},
+                {"valor": 1/72, "reps": ("𐆓","ƨ","𐆓","ƨ")},
+                {"valor": 1/144, "reps": ("𐆔","𐆔","𐆔","𐆔")},
+                {"valor": 1/288, "reps": ("℈","℈","℈","℈")},
+                {"valor": 1/1728, "reps": ("》","𐆕","》","𐆕")}
+            ]
+    symbols += fracs
 
     symbols = sorted(symbols, key= lambda d:d['valor'])
 
     return tuple(reversed(symbols))
 
-def pleaseJupiter():
+def getPleaseJupiter():
     return ({"valor": 4, "reps":("IIII", "iiii", "ⅠⅠⅠⅠ", "ⅰⅰⅰⅰ")})
 
-def clockFace():
+def getClockFace():
     return (
         {"valor": 1, "reps": ("I", "i",       "Ⅰ", "ⅰ")},
         {"valor": 2, "reps": ("II", "ii",     "Ⅱ", "ⅱ")},
@@ -115,3 +173,16 @@ def clockFace():
         {"valor": 11, "reps": ("XI", "xi",     "Ⅺ", "ⅺ")},
         {"valor": 12, "reps": ("XII", "xii",   "Ⅻ", "ⅻ")},
     )
+def sesuncia(prevchar, lowercase):
+    ses= ('Є' if not lowercase else 'є' ) + '·'
+    if prevchar == '·':
+        return ses
+    if prevchar == ':':
+        return '·' + ses
+    if prevchar == '∴':
+        return ':' + ses
+    if prevchar == '∷':
+        return '∴' + ses
+    if prevchar == '⁙':
+        return '∷' + ses
+    return prevchar + ('Є' if not lowercase else 'є' )
